cb7197f8fd18d55ed0252ba0f4882ed80a9da6aa
SpunBeat <olvera.alan@gmail.com>
2016-05-10 18:05:54 -0500

    Update Service Vash findOffsetInArray

--- app/modules/help/vash.service.js
+++ app/modules/help/vash.service.js
@@ -5,29 +5,57 @@
     var service = function () {
 
         var self = this;
-        
 
-        self.findOffsetInArray = function (arr, capacity, searchBy) {
-            
+
+        self.findOffsetInArray = function (arr, capacity, searchIn, searchBy) {
+
             this.offset = {};
+            this.searchIn = searchIn || '';
             this.searchBy = searchBy || 'name';
-            
-            for (var i in arr) {
-                var text = arr[i].text;
-                var ctext = capacity[this.searchBy];
-                
-                if (ctext !== undefined &&
-                    ctext.toUpperCase() == text.toUpperCase()) {
-                    this.offset = arr[i].offset;
+
+            var textInTheArray = '';
+            var textToMatch = '';
+
+            if (this.searchIn !== '') {
+                if (capacity[this.searchIn].length) {
+                    // Search In: Array
+                    for (var k in arr) {
+                        textInTheArray = arr[k].text;
+                        for (var l in capacity[this.searchIn]) {
+                            textToMatch = capacity[this.searchIn][l][this.searchBy];
+                            if (textToMatch !== undefined &&
+                                textToMatch.toUpperCase() == textInTheArray.toUpperCase()) {
+                                this.offset = arr[k].offset;
+                            }
+                        }
+                    }
+                } else {
+                    // Search In: Object
+                    for (var j in arr) {
+                        textInTheArray = arr[j].text;
+                        textToMatch = capacity[this.searchIn][this.searchBy];
+                        if (textToMatch !== undefined &&
+                            textToMatch.toUpperCase() == textInTheArray.toUpperCase()) {
+                            this.offset = arr[j].offset;
+                        }
+                    }
                 }
-                
+            } else {
+                // Search By
+                for (var i in arr) {
+                    textInTheArray = arr[i].text;
+                    textToMatch = capacity[this.searchBy];
+
+                    if (textToMatch !== undefined &&
+                        textToMatch.toUpperCase() == textInTheArray.toUpperCase()) {
+                        this.offset = arr[i].offset;
+                    }
+                }
+
             }
-            
+
             return this.offset;
         };
-        
-        
-        
 
         return self;
     };
